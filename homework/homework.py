684d0db89b56edeab1f4ab87f7dfb91fa774a1b6
Camila Arias <69543715+mcamilaariasb@users.noreply.github.com>
2025-01-23 20:12:37 -0500

    lab 5

--- homework/homework.py
+++ homework/homework.py
@@ -4,6 +4,9 @@ Escriba el codigo que ejecute la accion solicitada.
 
 # pylint: disable=import-outside-toplevel
 
+import os
+import zipfile
+import pandas as pd
 
 def clean_campaign_data():
     """
@@ -49,9 +52,52 @@ def clean_campaign_data():
 
 
     """
+    input_path = 'files/input/'
+    output_path = 'files/output/'
 
-    return
+    os.makedirs(output_path, exist_ok=True)
+    
+    for filename in ['client.csv', 'campaign.csv', 'economics.csv']:
+        if os.path.exists(output_path + filename):
+            os.remove(output_path + filename)
 
+    header_client = True
+    header_campaign = True
+    header_economics = True
 
+    for file in os.listdir(input_path):
+        if file.endswith('.zip'):
+
+            with zipfile.ZipFile(os.path.join(input_path, file), 'r') as z:
+                for csv_file in z.namelist():
+
+                    with z.open(csv_file) as f:
+                        df = pd.read_csv(f)
+                        client_df = df[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']].copy()
+                        client_df['job'] = client_df['job'].str.replace('.', '').str.replace('-', '_')
+                        client_df['education'] = client_df['education'].str.replace('.', '_').replace('unknown', pd.NA)
+                        client_df['credit_default'] = client_df['credit_default'].map({'yes': 1}).fillna(0)
+                        client_df['mortgage'] = client_df['mortgage'].map({'yes': 1}).fillna(0)
+                        client_df.to_csv(output_path + 'client.csv', index=False, mode='a', header=header_client)
+                        
+                        header_client = False
+
+                        campaign_df = df[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome', 'day', 'month']].copy()
+                        campaign_df['previous_outcome'] = campaign_df['previous_outcome'].map({'success': 1}).fillna(0)
+                        campaign_df['campaign_outcome'] = campaign_df['campaign_outcome'].map({'yes': 1}).fillna(0)
+                        campaign_df['last_contact_date'] = pd.to_datetime(
+                            '2022-' + campaign_df['month'] + '-' + campaign_df['day'].astype(str), 
+                            format='%Y-%b-%d'
+                        ).dt.strftime('%Y-%m-%d')
+                        campaign_df.drop(columns=['month', 'day'], inplace=True)
+                        campaign_df.to_csv(output_path + 'campaign.csv', index=False, mode='a', header=header_campaign)
+                        
+                        header_campaign = False
+
+                        economics_df = df[['client_id', 'cons_price_idx', 'euribor_three_months']].copy()
+                        economics_df.to_csv(output_path + 'economics.csv', index=False, mode='a', header=header_economics)
+                        
+                        header_economics = False
+                        
 if __name__ == "__main__":
     clean_campaign_data()
